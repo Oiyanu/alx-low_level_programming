c423707d79a993f063c202fb824a788ebfd910f4
Oiyanu <iyanuolatomide@gmail.com>
2023-09-07 16:02:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ * 
+ * Return - Always 0 (success)
+ */
+int nain(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
