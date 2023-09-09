7aa45cf89ba7862441e5d78df634b47d00f389df
Oiyanu <iyanuolatomide@gmail.com>
2023-09-08 18:23:15 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char y;
+
+	for (y = 'a'; y <= 'b' ; y++)
+	putchar (y);
+	putchar ('\n');
+	return (0);
+}
