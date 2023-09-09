25bcd1eff4d25be85c720ce5458807f06fe7c516
Oiyanu <iyanuolatomide@gmail.com>
2023-09-08 19:33:36 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,14 @@ int main(void)
 {
 	char alphabet = 'a';
 	while (alphabet <= 'z')
+		
 	{
 		if (alphabet != 'e' && alphabet != 'q')
 		{
 			putchar(alphabet);
 		}
 		alphabet++;
-	}
-	
+	}	
 	putchar('\n');
 	return (0);
 }
