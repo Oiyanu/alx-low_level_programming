4073f03bfa4d68e49f06e5e2108eb34d8fec2101
Oiyanu <iyanuolatomide@gmail.com>
2023-10-30 18:38:51 -0700

    update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		for (lenlett = 0; text_content[lenlett]; lenlett++)
-
+			;
 		lenwr = write(f_des, text_content, lenlett);
 
 		if (lenwr == -1)
